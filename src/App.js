32827acedf3f7c294b486151e0a898a3306bff97
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-10-02 23:17:50 +0100

    Decided the winner

--- src/App.js
+++ src/App.js
@@ -12,21 +12,52 @@ const App = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isONext, setONext] = useState(false);
 
+    const checkWinner = () => {
+        const winningCombination = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < winningCombination.length; i++) {
+            const el = winningCombination[i];
+            if (
+                squares[el[0]] != null &&
+                squares[el[0]] === squares[el[1]] &&
+                squares[el[1]] === squares[el[2]]
+            ) {
+                return squares[el[0]];
+            }
+            // console.log("el0 " + squares[el[0]]);
+        }
+        return false;
+    };
+
     const renderSquare = i => {
         return (
             <Square
                 val={squares[i]}
                 onClick={() => {
                     const nextSquareState = squares.slice();
-                    nextSquareState[i] = isONext ? "O" : "X";
-                    setONext(!isONext);
-                    setSquares(nextSquareState);
+                    const winner = checkWinner();
+                    if (nextSquareState[i] == null && !winner) {
+                        nextSquareState[i] = isONext ? "O" : "X";
+                        setONext(!isONext);
+                        setSquares(nextSquareState);
+                    }
                 }}
             />
         );
     };
+    const winner = checkWinner();
     return (
         <div>
+            {winner && <p>Winner {winner}</p>}
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
